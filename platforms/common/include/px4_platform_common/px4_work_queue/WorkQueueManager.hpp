0ca8a9bac906297ba2b6a5ca2ef9053142a65ed7
David Sidrane <David.Sidrane@NscDg.com>
2019-11-27 12:35:43 -0800

    WorkQueueManager:Fix low stack warning introduced by stism330

--- platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
+++ platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -52,7 +52,7 @@ static constexpr wq_config_t rate_ctrl{"wq:rate_ctrl", 1600, 0}; // PX4 inner lo
 
 static constexpr wq_config_t SPI0{"wq:SPI0", 1600, -1};
 static constexpr wq_config_t SPI1{"wq:SPI1", 1600, -2};
-static constexpr wq_config_t SPI2{"wq:SPI2", 1600, -3};
+static constexpr wq_config_t SPI2{"wq:SPI2", 1616, -3};
 static constexpr wq_config_t SPI3{"wq:SPI3", 1600, -4};
 static constexpr wq_config_t SPI4{"wq:SPI4", 1600, -5};
 static constexpr wq_config_t SPI5{"wq:SPI5", 1600, -6};
